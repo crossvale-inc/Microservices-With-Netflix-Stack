f65f7584a5322ce40d22cf592f9f5ed603ad2596
afeiszli <alex.feiszli@gmail.com>
2017-12-20 14:24:27 -0500

    added lab 0 folder and applications

---
+++ Lab00_Base_Applications/base.formatting.service/src/main/java/base/formatting/service/RESTController.java
@@ -0,0 +1,25 @@
+package base.formatting.service;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class RESTController {
+
+    @RequestMapping(value = "/formatter/", method = RequestMethod.POST)
+    public String returnMessage(@RequestBody Message message){
+        message.convertBody();
+        
+        RestTemplate restTemplate = new RestTemplate();
+        
+		return restTemplate.postForObject("http://localhost:8080/sendMessage/", message, String.class);
+    }
+	
+	public String defaultMessageResponse( Message message ) {
+		return "Send of " + message.toString() + " to formatter did not work.";
+	}
+	
+}
