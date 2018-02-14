981b93eb07c01823bbe3c67dcfb962db60dc0efb
afeiszli <alex.feiszli@gmail.com>
2018-02-14 15:06:21 -0500

    changed config props

--- Lab05_Config/lab5.formatting.service/src/main/java/lab5/formatting/service/Application.java
+++ Lab05_Config/lab5.formatting.service/src/main/java/lab5/formatting/service/Application.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableHystrix
 @EnableEurekaClient
-//@EnableResourceServer
+@EnableResourceServer
 public class Application {
 
     public static void main(String[] args) {
