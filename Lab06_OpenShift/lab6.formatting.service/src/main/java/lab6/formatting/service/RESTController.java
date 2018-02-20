f26041f422664af9d85c9e93dc8e592b00fc2ed6
afeiszli <alex.feiszli@gmail.com>
2018-02-20 10:40:55 -0600

    adding Lab 6

---
+++ Lab06_OpenShift/lab6.formatting.service/src/main/java/lab6/formatting/service/RESTController.java
@@ -0,0 +1,20 @@
+package lab6.formatting.service;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class RESTController {
+
+    @RequestMapping(value = "/formatter/", method = RequestMethod.POST)
+    public String returnMessage(@RequestBody Message message){
+        message.convertBody();
+        
+        RestTemplate restTemplate = new RestTemplate();
+        
+		return restTemplate.postForObject("http://localhost:8080/sendMessage/", message, String.class);
+    }
+}
